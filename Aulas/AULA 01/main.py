383c2ac94c01b26df3f90722984179a3f698192f
Jonathan gay <eu.jonatan777@gmail.com>
2026-08-27 10:24:07 -0300

    Delete Aulas directory

--- Aulas/AULA 01/main.py
+++
@@ -1,7 +0,0 @@
-import re
-codigo = input("Digite um código: ")
-if re.fullmatch(r"\d{4}", codigo):
-    print("Código válido! ")
-
-else:
-    print("Código inválido! ")
--- Aulas/main.py
+++
@@ -1,8 +0,0 @@
-frase = "Tatu bola"
-frase_nova = frase.replace("Tatu", "Cachorro")
-print(frase)
-print(frase_nova)
-
-preco = "R$10,00"
-limpo = preco.replace("R$", "")
-print(limpo)
